29284f7f0dd52700e439f74f89b127a7803b99ea
Ociel Lerma <oglerma@gmail.com>
2019-06-04 18:45:36 -0700

    Created a new instance of SCNScene(named: "PokerDice.scnassets/SimpleScene.scn")! in order to create my own 3d figure and display it using the camera.

--- Hello ARKit/Hello ARKit/ViewController.swift
+++ Hello ARKit/Hello ARKit/ViewController.swift
@@ -24,7 +24,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.showsStatistics = true
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+//        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = SCNScene(named: "PokerDice.scnassets/SimpleScene.scn")!
         
         // Set the scene to the view
         sceneView.scene = scene
